2d9c25fa83861149fd92f5888c7c61abeca58554
Tian Khamez <tkhamez@gmail.com>
2020-01-26 20:53:52 +0100

    Raised minimum PHP version to 7.2

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,6 +9,7 @@ use Slim\Interfaces\RouteParserInterface;
 use Slim\Psr7\Factory\ResponseFactory;
 use Slim\Routing\Dispatcher;
 use Slim\Routing\RouteCollector;
+use Slim\Routing\RouteContext;
 use Slim\Routing\RoutingResults;
 
 class TestCase extends \PHPUnit\Framework\TestCase
@@ -16,7 +17,7 @@ class TestCase extends \PHPUnit\Framework\TestCase
     protected function addRouteContext(ServerRequestInterface $request, string $path = null): ServerRequestInterface
     {
         $routeParser = $this->getMockBuilder(RouteParserInterface::class)->getMock();
-        $request = $request->withAttribute('routeParser', $routeParser);
+        $request = $request->withAttribute(RouteContext::ROUTE_PARSER, $routeParser);
 
         $routingResults = new RoutingResults(
             new Dispatcher(new RouteCollector(new ResponseFactory(),new CallableResolver())),
@@ -24,12 +25,12 @@ class TestCase extends \PHPUnit\Framework\TestCase
             '/',
             200
         );
-        $request = $request->withAttribute('routingResults', $routingResults);
+        $request = $request->withAttribute(RouteContext::ROUTING_RESULTS, $routingResults);
 
         if ($path) {
             $route = $this->getMockBuilder(RouteInterface::class)->getMock();
             $route->method('getPattern')->willReturn($path);
-            $request = $request->withAttribute('route', $route);
+            $request = $request->withAttribute(RouteContext::ROUTE, $route);
         }
 
         return $request;
